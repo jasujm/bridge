2ca4d68c88eb2e8e40a356ee2ad7aac7e9631a19
Jaakko Moisio <jaakko@moisio.fi>
2016-06-07 23:44:28 +0300

    Add MessageBuffer
    MessageBuffer is used to get STL stream interface to ZeroMQ. This is needed to
    adapt LibTMCG which uses streams for interactive proofs.

---
+++ include/messaging/MessageBuffer.hh
@@ -0,0 +1,121 @@
+/** \file
+ *
+ * \brief Definition of Bridge::Messaging::MessageBuffer
+ */
+
+#ifndef MESSAGING_MESSAGEBUFFER_HH_
+#define MESSAGING_MESSAGEBUFFER_HH_
+
+#include "messaging/MessageUtility.hh"
+
+#include <zmq.hpp>
+
+#include <sstream>
+#include <tuple>
+#include <utility>
+
+namespace Bridge {
+namespace Messaging {
+
+/** \brief String buffer that sends and receives messages through ZeroMQ sockets
+ *
+ * BasicMessageBuffer is derived from std::string_buffer and can be used like
+ * any string buffer (building and extracting data from string). When the
+ * output is synchronized, the contents of the buffer is sent through a ZeroMQ
+ * socket. When it underflows, new content is receivec from the socket. Sends
+ * and receives are blocking.
+ *
+ * The intended use of BasicMessageBuffer is to create input and output
+ * streams that act as frontends of ZeroMQ sockets.
+ *
+ * \tparam Char the character type
+ * \tparam Traits the character traits
+ * \tparam Allocator allocator for the string
+ *
+ * \sa MessageBuffer
+ */
+template<
+    typename Char,
+    class Traits = std::char_traits<Char>,
+    class Allocator = std::allocator<Char>>
+class BasicMessageBuffer :
+    public std::basic_stringbuf<Char, Traits, Allocator> {
+public:
+
+    /** \brief Create message buffer
+     *
+     * \param socket the socket used to synchronize contents
+     */
+    BasicMessageBuffer(zmq::socket_t socket);
+
+protected:
+
+    /** \brief Send current contents of the buffer
+     *
+     * Synchronizes the controlled character sequence by sending it through
+     * the socket given as parameter to BasicMessageBuffer(). This method is
+     * intended to be called by the output stream when it is flushed.
+     */
+    int sync() override;
+
+    /** \brief Retrieve new content for the buffer
+     *
+     * Ensures that there are character available by retrieving message
+     * through the socket given as parameter to BasicMessageBuffer() and
+     * filling the controlled character sequence by that message. This method
+     * is intended to be called by the input stream when it runs out of
+     * characters.
+     */
+    typename Traits::int_type underflow() override;
+
+private:
+
+    zmq::socket_t socket;
+};
+
+template<typename Char, class Traits, class Allocator>
+BasicMessageBuffer<Char, Traits, Allocator>::BasicMessageBuffer(
+    zmq::socket_t socket) :
+    socket {std::move(socket)}
+{
+}
+
+template<typename Char, class Traits, class Allocator>
+int BasicMessageBuffer<Char, Traits, Allocator>::sync()
+{
+    const auto& msg = this->str();
+    if (!msg.empty()) {
+        sendMessage(this->socket, msg);
+        this->str({});
+    }
+    return 0;
+}
+
+template<typename Char, class Traits, class Allocator>
+typename Traits::int_type BasicMessageBuffer<Char, Traits, Allocator>::
+underflow()
+{
+    const auto super = std::basic_stringbuf<
+        Char, Traits, Allocator>::underflow();
+    if (super != Traits::eof()) {
+        return super;
+    }
+
+    using String = std::basic_string<Char, Traits, Allocator>;
+    auto msg = String {};
+    auto more = false;
+    while (msg.empty()) {
+        std::tie(msg, more) = recvMessage<String>(this->socket);
+    }
+    this->str(msg);
+    return Traits::to_int_type(msg.front());
+}
+
+/** \brief BasicMessageBuffer specialization for regular char
+ */
+using MessageBuffer = BasicMessageBuffer<char>;
+
+}
+}
+
+#endif // MESSAGING_MESSAGEBUFFER_HH_
---
+++ test/src/messaging/TestMessageBuffer.cc
@@ -0,0 +1,77 @@
+#include "messaging/MessageBuffer.hh"
+#include "messaging/MessageUtility.hh"
+
+#include <gtest/gtest.h>
+#include <zmq.hpp>
+
+#include <array>
+#include <iostream>
+#include <string>
+#include <utility>
+
+namespace {
+using namespace std::string_literals;
+const auto ENDPOINT = "inproc://example"s;
+const auto MESSAGE = "message"s;
+const auto NUMBER = 123;
+const auto WHOLE_MESSAGE = "message 123\n"s;
+}
+
+using Bridge::Messaging::sendMessage;
+using Bridge::Messaging::recvMessage;
+using Bridge::Messaging::MessageBuffer;
+
+class MessageBufferTest : public testing::Test {
+protected:
+    virtual void SetUp()
+    {
+        backSocket.bind(ENDPOINT);
+        frontSocket.connect(ENDPOINT);
+    }
+
+    zmq::context_t context;
+    zmq::socket_t frontSocket {context, zmq::socket_type::pair};
+    zmq::socket_t backSocket {context, zmq::socket_type::pair};
+};
+
+TEST_F(MessageBufferTest, testOutputMessage)
+{
+    MessageBuffer buffer {std::move(frontSocket)};
+    std::ostream out {&buffer};
+    out << MESSAGE << " " << NUMBER << std::endl;
+
+    const auto message = recvMessage(backSocket);
+    EXPECT_EQ(std::make_pair(WHOLE_MESSAGE, false), message);
+}
+
+TEST_F(MessageBufferTest, testFlushEmptyOutputShouldNotSendMessage)
+{
+    MessageBuffer buffer {std::move(frontSocket)};
+    std::ostream out {&buffer};
+    out.flush();
+
+    std::array<zmq::pollitem_t, 1> pollitems {{
+        {static_cast<void*>(backSocket), 0, ZMQ_POLLIN, 0},
+    }};
+    EXPECT_EQ(0, zmq::poll(pollitems.data(), 1, 0));
+}
+
+TEST_F(MessageBufferTest, testInputMessage)
+{
+    sendMessage(frontSocket, WHOLE_MESSAGE);
+
+    MessageBuffer buffer {std::move(backSocket)};
+    std::istream in {&buffer};
+
+    {
+        std::string message {};
+        in >> message;
+        EXPECT_EQ(MESSAGE, message);
+    }
+
+    {
+        int num {};
+        in >> num;
+        EXPECT_EQ(NUMBER, num);
+    }
+}
