b575186087b3c0234517ae0b7b750da5e8ec964d
Jaakko Moisio <jaakko@moisio.fi>
2016-10-08 10:41:42 +0300

    Create Python module for messaging utilities
    The messaging module contains utility functions endpoints(), sendMessage() and
    MessageQueue for abstracting reply and event handling from the server.

---
+++ test/python/bridgegui/test_messaging.py
@@ -0,0 +1,75 @@
+import unittest
+
+from PyQt5.QtTest import QSignalSpy
+import zmq.decorators
+import zmq
+
+from bridgegui.messaging import (
+    endpoints, sendCommand, MessageQueue, REPLY_SUCCESS_PREFIX)
+
+ENDPOINT = 'inproc://testing'
+COMMAND = b'command'
+
+
+class MessagingTest(unittest.TestCase):
+    """Unit test suite for messaging"""
+
+    def testEndpoints(self):
+        gen = endpoints("tcp://127.0.0.1:5555")
+        self.assertEqual(next(gen), "tcp://127.0.0.1:5555")
+        self.assertEqual(next(gen), "tcp://127.0.0.1:5556")
+
+
+class MessageQueueTest(unittest.TestCase):
+    """Unit test suite for message queue"""
+
+    def setUp(self):
+        self._zmqctx = zmq.Context()
+        self._back_socket = self._zmqctx.socket(zmq.PAIR)
+        self._back_socket.bind(ENDPOINT)
+        self._front_socket = self._zmqctx.socket(zmq.PAIR)
+        self._front_socket.connect(ENDPOINT)
+        self._message_queue = MessageQueue(
+            self._back_socket, "test message queue",
+            REPLY_SUCCESS_PREFIX, { COMMAND: self._handle_command })
+        self._command_handled = False
+
+    def tearDown(self):
+        self._zmqctx.destroy()
+
+    def testSendCommand(self):
+        sendCommand(self._front_socket, COMMAND, arg=1)
+        self.assertEqual(
+            self._back_socket.recv_multipart(flags=zmq.NOBLOCK),
+            [b'', COMMAND, b'arg', b'1'])
+
+    def testIncorrectPrefix(self):
+        self._front_socket.send_multipart((b'this', b'is', b'incorrect'))
+        self.assertFalse(self._message_queue.handleMessages())
+
+    def testMissingCommand(self):
+        self._front_socket.send_multipart(REPLY_SUCCESS_PREFIX)
+        self.assertFalse(self._message_queue.handleMessages())
+
+    def testIncorrectCommand(self):
+        self._front_socket.send_multipart(REPLY_SUCCESS_PREFIX + [b'incorrect'])
+        self.assertFalse(self._message_queue.handleMessages())
+
+    def testIncorrectSerialization(self):
+        self._front_socket.send_multipart(REPLY_SUCCESS_PREFIX + [b'incorrect'])
+        self.assertFalse(self._message_queue.handleMessages())
+
+    def testIncorrectArgument(self):
+        self._front_socket.send_multipart(
+            REPLY_SUCCESS_PREFIX + [COMMAND, b'arg', b'not json'])
+        self.assertFalse(self._message_queue.handleMessages())
+
+    def testHandleMessage(self):
+        self._front_socket.send_multipart(
+            REPLY_SUCCESS_PREFIX + [COMMAND, b'arg', b'123'])
+        self.assertTrue(self._message_queue.handleMessages())
+        self.assertTrue(self._command_handled)
+
+    def _handle_command(self, arg):
+        self.assertEqual(arg, 123)
+        self._command_handled = True
