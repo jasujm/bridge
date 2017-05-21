bb5287f740e99865da1db36736d0387d230b0686
Jaakko Moisio <jaakko@moisio.fi>
2017-05-21 20:21:15 +0300

    Add NodePlayerControl
    NodePlayerControl is a simpler replacement for NodeControl that supports
    identifying players with UUID.

---
+++ include/main/NodePlayerControl.hh
@@ -0,0 +1,79 @@
+/** \file
+ *
+ * \brief Definition of Bridge::Main::NodePlayerControl
+ */
+
+#ifndef MAIN_NODEPLAYERCONTROL_HH_
+#define MAIN_NODEPLAYERCONTROL_HH_
+
+#include "bridge/Player.hh"
+
+#include <boost/core/noncopyable.hpp>
+#include <boost/optional/optional.hpp>
+
+#include <memory>
+#include <string>
+
+namespace Bridge {
+
+class Player;
+
+namespace Main {
+
+/** \brief Utility class for access control of nodes and players
+ *
+ * Each node in the bridge protocol is allowed to control one or multiple
+ * players in each game. This class is used for the access control.
+ */
+class NodePlayerControl : private boost::noncopyable {
+public:
+
+    /** \brief Create new node player control object
+     */
+    NodePlayerControl();
+
+    ~NodePlayerControl();
+
+    /** \brief Create player controlled by a node
+     *
+     * This method is used request a new player to be controlled by \p node and
+     * identified by \p uuid. The combination of \p node and \p uuid can later
+     * be used to check if the node is allowed to act for the player.
+     *
+     * The method returns nullptr if \p uuid was already used to create a
+     * player.
+     *
+     * \param node identity of the node controlling or representing the player
+     * \param uuid UUID of the new player
+     */
+    std::shared_ptr<Player> createPlayer(
+        const std::string& node, const boost::optional<Uuid>& uuid);
+
+    /** \brief Get player if the node is allowed to act for itself
+     *
+     * This method is used to check if \p node is allowed to act for a player
+     * identified by \p uuid. If a player was previously created with the same
+     * parameters, a pointer pointing to it is returned. Otherwise nullptr is
+     * returned.
+     *
+     * The \p uuid parameter may also be none, in which case pointer to a valid
+     * pointer is returned if and only if \p node controls exactly one player.
+     *
+     * \param node identity of the node controlling or representing the player
+     * \param uuid UUID of the player
+     */
+    std::shared_ptr<const Player> getPlayer(
+        const std::string& node,
+        const boost::optional<Uuid>& uuid = boost::none) const;
+
+private:
+
+    class Impl;
+    const std::unique_ptr<Impl> impl;
+};
+
+
+}
+}
+
+#endif // MAIN_NODEPLAYERCONTROL_HH_
---
+++ src/main/NodePlayerControl.cc
@@ -0,0 +1,100 @@
+#include "main/NodePlayerControl.hh"
+
+#include "bridge/BasicPlayer.hh"
+#include "Utility.hh"
+
+#include <boost/uuid/random_generator.hpp>
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace Bridge {
+namespace Main {
+
+class NodePlayerControl::Impl {
+public:
+    std::shared_ptr<Player> createPlayer(
+        const std::string& node, const boost::optional<Uuid>& uuid);
+
+    std::shared_ptr<const Player> getPlayer(
+        const std::string& node, const boost::optional<Uuid>& uuid) const;
+
+private:
+
+    struct Element {
+        std::string node;
+        std::shared_ptr<Player> player;
+    };
+
+    // Assumption: access control happens per game and thus the vector contains
+    // small number of elements. Searching the vector is fast.
+    std::vector<Element> players;
+    boost::uuids::random_generator uuidGenerator;
+};
+
+NodePlayerControl::NodePlayerControl() :
+    impl {std::make_unique<Impl>()}
+{
+}
+
+std::shared_ptr<Player> NodePlayerControl::Impl::createPlayer(
+    const std::string& node, const boost::optional<Uuid>& uuid)
+{
+    const auto uuid_for_player = uuid ? *uuid : uuidGenerator();
+    const auto iter = std::find_if(
+        players.begin(), players.end(),
+        [&uuid_for_player](const auto& e)
+        {
+            return dereference(e.player).getUuid() == uuid_for_player;
+        });
+    if (iter == players.end()) {
+        const auto ret = std::make_shared<BasicPlayer>(uuid_for_player);
+        players.emplace_back(Element {node, ret});
+        return ret;
+    }
+    return nullptr;
+}
+
+std::shared_ptr<const Player> NodePlayerControl::Impl::getPlayer(
+    const std::string& node, const boost::optional<Uuid>& uuid) const
+{
+    const auto last = players.end();
+    if (uuid) {
+        const auto iter = std::find_if(
+            players.begin(), last,
+            [&uuid](const auto& e)
+            {
+                return dereference(e.player).getUuid() == uuid;
+            });
+        if (iter != last && iter->node == node) {
+            return iter->player;
+        }
+    } else {
+        const auto func = [&node](const auto& e) { return e.node == node; };
+        const auto iter = std::find_if(players.begin(), last, func);
+        if (iter != last && std::find_if(iter + 1, last, func) == last) {
+            return iter->player;
+        }
+    }
+    return nullptr;
+}
+
+NodePlayerControl::~NodePlayerControl() = default;
+
+std::shared_ptr<Player> NodePlayerControl::createPlayer(
+    const std::string& node, const boost::optional<Uuid>& uuid)
+{
+    assert(impl);
+    return impl->createPlayer(node, uuid);
+}
+
+std::shared_ptr<const Player> NodePlayerControl::getPlayer(
+    const std::string& node, const boost::optional<Uuid>& uuid) const
+{
+    assert(impl);
+    return impl->getPlayer(node, uuid);
+}
+
+}
+}
