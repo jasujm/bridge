c574e84d30cad8bac5e865aabfc730856277e234
Jaakko Moisio <jaakko@moisio.fi>
2019-02-21 21:13:28 +0100

    Simplify MessageQueue control flow
    This commit introduces to simplifications to the implementation of MessageQueue:
    
    - All reply frames are first collected into a vector in BasicResponse object and
      then sent at once as one multipart message. BasicResponse manipulates this
      vector directly when setting status and adding frames.
    
    - Failing when no message handler is available is handled through default
      message handler (variation of null object pattern).

--- src/messaging/MessageQueue.cc
+++ src/messaging/MessageQueue.cc
@@ -20,22 +20,72 @@ using MessageVector = std::vector<zmq::message_t>;
 
 class BasicResponse : public Response {
 public:
+
+    template<typename MessageIterator>
+    BasicResponse(
+        MessageIterator firstPrefix, MessageIterator lastPrefix,
+        zmq::message_t& commandFrame);
+
+    void sendResponse(zmq::socket_t& socket);
+
+private:
     void handleSetStatus(StatusCode status) override;
     void handleAddFrame(ByteSpan frame) override;
 
-    StatusCode status {REPLY_FAILURE};
-    MessageVector frames {};
+    std::size_t nStatusFrame;
+    MessageVector frames;
 };
+
+template<typename MessageIterator>
+BasicResponse::BasicResponse(
+    MessageIterator firstPrefix, MessageIterator lastPrefix,
+    zmq::message_t& commandFrame) :
+    // the first frame after the prefix is the status frame
+    // thus the prefix length is status frame index
+    nStatusFrame {
+        static_cast<std::size_t>(std::distance(firstPrefix, lastPrefix))},
+    frames(nStatusFrame + 2)
+{
+    auto replyIter = frames.begin();
+    while (firstPrefix != lastPrefix) {
+        replyIter->move(&(*firstPrefix));
+        ++replyIter; ++firstPrefix;
+    }
+    frames.back().move(&commandFrame);
+}
+
+void BasicResponse::sendResponse(zmq::socket_t& socket)
+{
+    sendMultipart(socket, frames.begin(), frames.end());
+}
 
 void BasicResponse::handleSetStatus(StatusCode status)
 {
-    this->status = status;
+    assert(nStatusFrame < frames.size());
+    const auto data = boost::endian::native_to_big(status);
+    frames[nStatusFrame].rebuild(&data, sizeof(data));
 }
 
 void BasicResponse::handleAddFrame(ByteSpan frame)
 {
-    this->frames.emplace_back(frame.data(), frame.size());
+    frames.emplace_back(frame.data(), frame.size());
 }
+
+class DefaultMessageHandler : public MessageHandler {
+private:
+    void doHandle(
+        SynchronousExecutionPolicy& execution, const Identity& identity,
+        const ParameterVector& params, Response& response) override;
+};
+
+void DefaultMessageHandler::doHandle(
+    SynchronousExecutionPolicy&, const Identity&, const ParameterVector&,
+    Response& response)
+{
+    response.setStatus(REPLY_FAILURE);
+}
+
+auto DEFAULT_MESSAGE_HANDLER = DefaultMessageHandler {};
 
 }
 
@@ -63,56 +113,42 @@ void MessageQueue::operator()(zmq::socket_t& socket)
     recvMultipart(socket, std::back_inserter(input_frames));
 
     auto* identity_msg = static_cast<zmq::message_t*>(nullptr);
-    auto first_payload_frame = input_frames.begin();
+    auto payload_frame_iter = input_frames.begin();
     const auto type = socket.getsockopt<zmq::socket_type>(ZMQ_TYPE);
     if (type == zmq::socket_type::router) {
-        first_payload_frame = std::find_if(
-            first_payload_frame, input_frames.end(),
+        payload_frame_iter = std::find_if(
+            payload_frame_iter, input_frames.end(),
             [](const auto& message) { return message.size() == 0u; });
         // If identity frame missing, silently drop it and don't reply
-        if (first_payload_frame == input_frames.begin()) {
+        if (payload_frame_iter == input_frames.begin()) {
             return;
         }
-        identity_msg = &first_payload_frame[-1];
+        identity_msg = &payload_frame_iter[-1];
         // If the empty frame is missing, silently drop it and don't reply
-        if (first_payload_frame == input_frames.end()) {
+        if (payload_frame_iter == input_frames.end()) {
             return;
         }
-        ++first_payload_frame;
+        ++payload_frame_iter;
     }
 
     // If command is missing, silently drop the message and don't reply
-    if (first_payload_frame == input_frames.end()) {
+    if (payload_frame_iter == input_frames.end()) {
         return;
     }
-    const auto command = messageView(*first_payload_frame);
-    const auto command_handler_entry = handlers.find(command);
-    auto response = BasicResponse {};
-    if (command_handler_entry != handlers.end()) {
-        auto& handler = dereference(command_handler_entry->second);
-        auto execution = SynchronousExecutionPolicy {};
-        handler.handle(
-            execution, identityFromMessage(*first_payload_frame, identity_msg),
-            first_payload_frame+1, input_frames.end(), response);
-    }
 
-    // Echo back everything before first payload, i.e. routing info
-    sendMultipart(
-        socket, input_frames.begin(), first_payload_frame, ZMQ_SNDMORE);
+    const auto handler_iter = handlers.find(messageView(*payload_frame_iter));
+    auto& handler = (handler_iter != handlers.end()) ?
+        dereference(handler_iter->second) : DEFAULT_MESSAGE_HANDLER;
+    auto execution = SynchronousExecutionPolicy {};
+    const auto identity = identityFromMessage(
+        *payload_frame_iter, identity_msg);
+    auto response = BasicResponse(
+        input_frames.begin(), payload_frame_iter, *payload_frame_iter);
+    handler.handle(
+        execution, identity, payload_frame_iter+1, input_frames.end(),
+        response);
 
-    // Send status
-    socket.send(
-        messageFromValue(boost::endian::native_to_big(response.status)),
-        ZMQ_SNDMORE);
-
-    // Send command
-    socket.send(
-        *first_payload_frame, response.frames.empty() ? 0 : ZMQ_SNDMORE);
-
-    // Send all output
-    if (!response.frames.empty()) {
-        sendMultipart(socket, response.frames.begin(), response.frames.end());
-    }
+    response.sendResponse(socket);
 }
 
 }
