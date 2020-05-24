4d1d2063f659f7b7fc0d38997ffee8552564e9d1
Jaakko Moisio <jaakko@moisio.fi>
2020-05-24 23:14:55 +0200

    Add imported target for LibTMCG
    Modernize FindLibTMCG a bit by exposing the library via target.

--- cmake/FindLibTMCG.cmake
+++ cmake/FindLibTMCG.cmake
@@ -1,10 +1,27 @@
-find_library(TMCG_LIBRARY TMCG)
-find_path(TMCG_INCLUDE_DIR libTMCG.hh)
+# Modeled after the example in the official docs:
+# https://cmake.org/cmake/help/v3.11/manual/cmake-developer.7.html
 
-set(TMCG_LIBRARIES ${TMCG_LIBRARY})
-set(TMCG_INCLUDE_DIRS ${TMCG_INCLUDE_DIR})
+find_path(LIBTMCG_INCLUDE_DIR
+  NAMES libTMCG.hh)
+
+find_library(LIBTMCG_LIBRARY
+  NAMES TMCG)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibTMCG DEFAULT_MSG TMCG_LIBRARY
-  TMCG_INCLUDE_DIR)
-mark_as_advanced(TMCG_LIBRARY TMCG_INCLUDE_DIR)
+find_package_handle_standard_args(LibTMCG
+  FOUND_VAR LIBTMCG_FOUND
+  REQUIRED_VARS LIBTMCG_LIBRARY LIBTMCG_INCLUDE_DIR)
+
+if (LIBTMCG_FOUND)
+  set(LIBTMCG_LIBRARIES ${LIBTMCG_LIBRARY})
+  set(LIBTMCG_INCLUDE_DIRS ${LIBTMCG_INCLUDE_DIR})
+endif()
+
+if (LIBTMCG_FOUND AND NOT TARGET LibTMCG::LibTMCG)
+  add_library(LibTMCG::LibTMCG UNKNOWN IMPORTED)
+  set_target_properties(LibTMCG::LibTMCG PROPERTIES
+    IMPORTED_LOCATION "${LIBTMCG_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${LIBTMCG_INCLUDE_DIR}")
+endif()
+
+mark_as_advanced(LIBTMCG_LIBRARY LIBTMCG_INCLUDE_DIR)
