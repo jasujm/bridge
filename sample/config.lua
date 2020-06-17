98ab1217da675fa645a1df8cfbc5bea19a95df60
Jaakko Moisio <jaakko@moisio.fi>
2020-06-17 13:50:23 +0200

    Clarify comments in the sample/config.lua script

--- sample/config.lua
+++ sample/config.lua
@@ -1,50 +1,60 @@
 -- Sample configuration file for the bridge application
 
--- Bind endpoint. Control socket binds to bind_base_port. Event socket binds to
--- bind_base_port + 1.
+-- The bind interface and port. Control socket binds to
+-- bind_base_port. Event socket binds to bind_base_port + 1.
 
 bind_address = os.getenv("BRIDGE_BIND_ADDRESS") or "*"
 bind_base_port = os.getenv("BRIDGE_BIND_BASE_PORT") or 5555
 
--- CurveZMQ keys to be used for communication between bridge nodes
--- NOTE! These are test keys (http://api.zeromq.org/4-2:zmq-curve) which by
--- definition are known keys. They cannot be used to authenticate nodes.
--- Set BRIDGE_USE_CURVE environment variable to use CURVE mechanism.
+-- Set curve_secret_key and curve_public_key to enable encrypting
+-- traffic with CurveZMQ. The keys here are the test keys test keys
+-- (http://api.zeromq.org/4-2:zmq-curve) and should be substituted for
+-- a keypair you generated.
 
 if os.getenv("BRIDGE_USE_CURVE") then
    curve_secret_key = "JTKVSB%%)wK0E.X)V>+}o?pNmC{O&4W4b!Ni{Lh6"
    curve_public_key = "rq:rM>}U?@Lns47E1%kR.o@n%FcmmsL/@{H8]yf7"
 end
 
--- With UUID only the game is peerless (clients only) and the backend controls
--- all positions. Uncomment necessary keyword arguments to configure game with
--- peers.
+-- Set up game(s) using the game function. The simple setup if for
+-- peerless games, in which case it's enough to provide uuid to
+-- identify the game. To setup a game with peers, you additionally
+-- need to specify positions_controlled, peers and, optionally,
+-- card_server.
+
+-- Note that peerless games can also be created by the frontend
+-- application after startup.
 
 game {
    uuid = "61b431d1-386b-4b16-a99d-d48dd35f1a4e",
 
-   -- For a game with peers you need to configure which positions this peer
-   -- controls. The bridge backend application does not autonegotiate positions
-   -- so everything needs to be agreed in advance.
+   -- For a game with peers you need to configure which positions this
+   -- peer controls. The positions need to be agreed in advance, and
+   -- together the peers need to control all positions. That is: each
+   -- player needs to uncomment exactly one of the lines below.
 
-   -- positions_controlled = { "north" }
+   -- positions_controlled = { "north" },
+   -- positions_controlled = { "east" },
+   -- positions_controlled = { "west" },
+   -- positions_controlled = { "south" },
 
-   -- List peers to connect to. Ensure that the other peers are configured to
-   -- cover all positions and only one position is controlled by one peer. This
-   -- also sets all peers to have the same curve server key as this node. It's
-   -- not necessary.
+   -- Tell the backend where it can finds its peers. Assuming all the
+   -- other peers have the same CURVE keypair as yourself (needs not
+   -- be the case), you can just assign server_key from the
+   -- curve_public_key you defined above.
 
    -- peers = {
-   --    { endpoint="tcp://peer1.example.com", server_key = curve_public_key },
-   --    { endpoint="tcp://peer2.example.com", server_key = curve_public_key },
-   --    { endpoint="tcp://peer3.example.com", server_key = curve_public_key },
+   --    { endpoint="tcp://peer1.example.com:5555", server_key = curve_public_key },
+   --    { endpoint="tcp://peer2.example.com:5555", server_key = curve_public_key },
+   --    { endpoint="tcp://peer3.example.com:5555", server_key = curve_public_key },
    -- },
 
-   -- To configure card exchange using card server, uncomment the following
-   -- section. Bridge backend does not start the card server process, so it must
-   -- be started separately with the same control and base peer endpoints
-   -- defined here. Peer endpoints need to be accessible by peers. For control
-   -- endpoint it's not necessary or even advisable.
+   -- To configure card exchange using card server, uncomment the
+   -- following section. control_endpoint and base_peer_endpoint are
+   -- what you use as command line arguments when running the bridgecs
+   -- command. Peer endpoints need to be accessible by the card server
+   -- peers. control_endpoint should only be accessible by the bridge
+   -- backend.
 
    -- card_server = os.getenv("BRIDGE_USE_CS") and {
    --    control_endpoint = "tcp://127.0.0.1:5560",
